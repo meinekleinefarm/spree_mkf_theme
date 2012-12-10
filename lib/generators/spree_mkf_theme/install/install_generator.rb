566a433a031fbc972068dca2e86f85abd5e034d6
Christoph Bünte <info@christophbuente.de>
2012-12-10 14:01:46 +0100

    Add twitter bootstrap support.

--- lib/generators/spree_mkf_theme/install/install_generator.rb
+++ lib/generators/spree_mkf_theme/install/install_generator.rb
@@ -4,12 +4,10 @@ module SpreeMkfTheme
 
       def add_javascripts
         append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_mkf_theme\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_mkf_theme\n"
       end
 
       def add_stylesheets
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_mkf_theme\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_mkf_theme\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
--- lib/spree_mkf_theme.rb
+++ lib/spree_mkf_theme.rb
@@ -1,2 +1,3 @@
 require 'spree_core'
 require 'spree_mkf_theme/engine'
+require 'bootstrap-sass'
