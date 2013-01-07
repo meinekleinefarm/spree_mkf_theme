d8b36db3908ca4975346e1c27fcce2f4b12ef226
Christoph Bünte <info@christophbuente.de>
2013-01-07 18:25:59 +0100

    Remove login links from header first.

--- app/overrides/remove_searchbar_from_header.rb
+++ app/overrides/remove_searchbar_from_header.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
                      :name => 'remove_searchbar_from_header',
-                     :remove => "code[erb-loud]:contains('spree/shared/search')",
-                     :original => '2361eded89274b5234da712ed05325ee1b8dadfa')
+                     :remove => "li#search-bar",
+                     :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac',
+                     :sequence => 1)
