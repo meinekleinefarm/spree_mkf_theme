02b4cd4024ef558aad47f39f11a5e16bbf520245
Christoph Bünte <info@christophbuente.de>
2013-01-14 17:27:30 +0100

    Move sidebar left and right on width change.

--- app/assets/javascripts/store/spree_mkf_theme.js
+++ app/assets/javascripts/store/spree_mkf_theme.js
@@ -1,1 +1,15 @@
-//= require store/spree_core
+//= require store/enquire
+$(function() {
+  enquire.register("screen and (max-width: 767px)", [{
+    setup : function() {
+      $('#content').before($('aside#sidebar'));
+    },
+    match : function() {
+      $('#content').before($('aside#sidebar'));
+    }
+  }]).register("screen and (min-width: 768px)", [{
+    match : function() {
+      $('#content').after($('aside#sidebar'));
+    }
+  }]).listen();
+});
