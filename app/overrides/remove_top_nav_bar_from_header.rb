b452abf1b889e834cc35c9e07a466bdd91cf1986
Christoph Bünte <info@christophbuente.de>
2013-01-15 15:26:18 +0100

    Add styling and overrides for checkout step one.

--- app/overrides/remove_top_nav_bar_from_header.rb
+++ app/overrides/remove_top_nav_bar_from_header.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
-                     :name => 'remove_searchbar_from_header',
-                     :remove => "li#search-bar",
+                     :name => 'remove_top_nav_bar_from_header',
+                     :remove => "#top-nav-bar",
                      :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac',
                      :sequence => 1)
