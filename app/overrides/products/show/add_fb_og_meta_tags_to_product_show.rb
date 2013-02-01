40cd69f790a64673142e7a73d95649fd84843c82
Christoph Bünte <info@christophbuente.de>
2013-02-01 20:55:07 +0100

    Remove social sharing links, as they are part of a plugin.

--- app/overrides/products/show/add_fb_og_meta_tags_to_product_show.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'add_fb_og_meta_tags_to_product_show',
-                     :insert_bottom => 'div[data-hook="product_show"]',
-                     :partial => 'spree/products/fb_og_meta_tags')
