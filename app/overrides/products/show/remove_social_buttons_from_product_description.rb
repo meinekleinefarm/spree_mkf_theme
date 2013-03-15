8bcf6b7b753eb3c593c367939f4f2885d51c1e64
Christoph Bünte <info@christophbuente.de>
2013-03-15 16:20:54 +0100

    Add sidebar to schweine detail page.

--- app/overrides/products/show/remove_social_buttons_from_product_description.rb
+++ app/overrides/products/show/remove_social_buttons_from_product_description.rb
@@ -1,7 +1,10 @@
 # Do nothing this overrides the original override from spree_social_products
 Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'remove_social_buttons_from_product_description',
-                     :remove => "code[erb-loud]:contains('social_buttons')")
+                     :remove => "[data-hook='product_left_part'] #social_nav",
+                     :sequence => {
+                       :after => 'add_social_buttons_to_products_show'
+                     })
 
 
 
