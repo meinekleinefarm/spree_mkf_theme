6b268485d9b04039844be6967116fe970048d70b
Christoph Bünte <info@christophbuente.de>
2013-03-15 11:08:12 +0100

    Remove facebook link from product index pages.

--- app/overrides/products/show/remove_social_buttons_from_product_description.rb
+++ app/overrides/products/show/remove_social_buttons_from_product_description.rb
@@ -1,6 +1,6 @@
 # Do nothing this overrides the original override from spree_social_products
 Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'add_social_buttons_to_products_show',
+                     :name => 'remove_social_buttons_from_product_description',
                      :remove => "code[erb-loud]:contains('social_buttons')")
 
 
