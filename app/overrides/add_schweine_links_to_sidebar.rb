1186d07569eca48cf7736992be3d269d923c8687
Christoph Bünte <info@christophbuente.de>
2013-03-18 14:28:47 +0100

    Add schweine links to sidebar on product index page.

--- app/overrides/add_schweine_links_to_sidebar.rb
+++ app/overrides/add_schweine_links_to_sidebar.rb
@@ -1,4 +1,9 @@
 Deface::Override.new(:virtual_path => 'spree/home/index',
-                     :name => 'add_schweine_links_to_sidebar',
+                     :name => 'add_schweine_links_to_sidebar_on_home_page',
                      :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
                      :partial => 'spree/schweine/sidebar')
+
+Deface::Override.new(:virtual_path => 'spree/products/index',
+                    :name => 'add_schweine_links_to_sidebar_on_products_page',
+                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :partial => 'spree/schweine/sidebar')
