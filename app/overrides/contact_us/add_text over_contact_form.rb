21114db1c6d2fbecd30e050ca0cb33daade1613c
Christoph Bünte <info@christophbuente.de>
2013-03-22 15:49:43 +0100

    Add styling for contact form.

---
+++ app/overrides/contact_us/add_text over_contact_form.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => 'spree/contact_us/contacts/new',
+                     :name => 'add_sidebar_to_product_page',
+                     :insert_after => 'h1',
+                     :text => '<p>Ich freu mich auf Deine Fragen und Anmerkungen.</p>',
+                     :original => '07972006d69d8eea10adec480b3bed6fcaa18573')
