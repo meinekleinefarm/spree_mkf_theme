8cf81958464b510a5666bdc9425950563df91d4f
Christoph Bünte <info@christophbuente.de>
2013-03-25 15:49:38 +0100

    Add styling fixes for the footer.

--- app/assets/javascripts/store/spree_mkf_theme.js
+++ app/assets/javascripts/store/spree_mkf_theme.js
@@ -1,6 +1,5 @@
 //= require store/enquire
 $(function() {
-
   $('input[type="radio"]').addClass('radio')
   enquire.register("screen and (max-width: 767px)", [{
     setup : function() {
@@ -12,6 +11,13 @@ $(function() {
   }]).register("screen and (min-width: 768px)", [{
     match : function() {
       $('#content').after($('aside#sidebar'));
+    }
+  }]).register("screen and (min-width: 960px)", [{
+    match : function() {
+      $('#content').removeClass('omega');
+    },
+    unmatch : function() {
+      $('#content').addClass('omega');
     }
   }]).listen();
 });
