ac1b21cddabfd157ee3fb15be7743a3b75a243a5
Christoph Bünte <info@christophbuente.de>
2013-08-01 13:03:02 +0200

    Add schweinelinks to homepage and product page.

--- app/overrides/add_schweine_links_to_sidebar.rb
+++ app/overrides/add_schweine_links_to_sidebar.rb
@@ -1,9 +1,27 @@
+# Original Spree Homepage
 Deface::Override.new(:virtual_path => 'spree/home/index',
                      :name => 'add_schweine_links_to_sidebar_on_home_page',
                      :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
                      :partial => 'spree/schweine/sidebar')
 
+
+# Spree Essential CMS Hompage
+Deface::Override.new(:virtual_path => 'spree/pages/home',
+                    :name => 'add_schweine_links_to_sidebar_on_home_page',
+                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :partial => 'spree/schweine/sidebar')
+
+# Spree Products index
 Deface::Override.new(:virtual_path => 'spree/products/index',
                     :name => 'add_schweine_links_to_sidebar_on_products_page',
                     :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
                     :partial => 'spree/schweine/sidebar')
+
+# Spree Products show
+Deface::Override.new(:virtual_path => 'spree/products/show',
+                    :name => 'add_schweine_links_to_sidebar_on_product_page',
+                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :partial => 'spree/schweine/sidebar',
+                    :sequence => {
+                      :after => 'add_sidebar_to_product_page'
+                    })
