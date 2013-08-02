3ba44444838f2fdadc401a64edbfab24b81b7b4e
Christoph Bünte <info@christophbuente.de>
2013-08-02 12:38:09 +0200

    Add scheine links to sidebar and move it up to the top.

--- app/overrides/add_schweine_links_to_sidebar.rb
+++ app/overrides/add_schweine_links_to_sidebar.rb
@@ -1,28 +1,36 @@
 # Original Spree Homepage
 Deface::Override.new(:virtual_path => 'spree/home/index',
                      :name => 'add_schweine_links_to_sidebar_on_home_page',
-                     :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                     :insert_top => '[data-hook="homepage_sidebar_navigation"]',
                      :partial => 'spree/schweine/sidebar')
-
 
 # Spree Essential CMS Hompage
 Deface::Override.new(:virtual_path => 'spree/pages/home',
                     :name => 'add_schweine_links_to_sidebar_on_home_page',
-                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :insert_top => '[data-hook="homepage_sidebar_navigation"]',
                     :partial => 'spree/schweine/sidebar')
 
 # Spree Products index
 Deface::Override.new(:virtual_path => 'spree/products/index',
                     :name => 'add_schweine_links_to_sidebar_on_products_page',
-                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :insert_top => '[data-hook="homepage_sidebar_navigation"]',
                     :partial => 'spree/schweine/sidebar')
 
 # Spree Products show
 Deface::Override.new(:virtual_path => 'spree/products/show',
                     :name => 'add_schweine_links_to_sidebar_on_product_page',
-                    :insert_bottom => '[data-hook="homepage_sidebar_navigation"]',
+                    :insert_top => '[data-hook="homepage_sidebar_navigation"]',
                     :partial => 'spree/schweine/sidebar',
                     :original => 'd9d1b3d18721e1c68eeaac898ca006bf8afb176c',
+                    :sequence => {
+                      :after => 'add_sidebar_to_product_page'
+                    })
+
+# Spree Taxon page
+Deface::Override.new(:virtual_path => 'spree/taxons/show',
+                    :name => 'add_schweine_links_to_sidebar_on_taxon_page',
+                    :insert_top => '[data-hook="taxon_sidebar_navigation"]',
+                    :partial => 'spree/schweine/sidebar',
                     :sequence => {
                       :after => 'add_sidebar_to_product_page'
                     })
