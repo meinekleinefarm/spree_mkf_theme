e671bcc359662b7bdfc4c81fb42720556447dd87
Christoph Bünte <info@christophbuente.de>
2013-08-19 17:01:46 +0200

    Render List of Bauern in sidebar.

--- app/overrides/products/show/add_class_to_variant_list.rb
+++ app/overrides/products/show/add_class_to_variant_list.rb
@@ -5,6 +5,20 @@ Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
                        :class => 'variant-list'
                      })
 
+Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
+                    :name => 'add_four_column_class_to_variant_list',
+                    :add_to_attributes => '#product-variants',
+                    :attributes => {
+                      :class => 'four'
+                    })
+Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
+                    :name => 'remove_five_column_class_to_variant_list',
+                    :remove_from_attributes => '#product-variants',
+                    :attributes => {
+                      :class => 'five'
+                    })
+
+
 
 
 
