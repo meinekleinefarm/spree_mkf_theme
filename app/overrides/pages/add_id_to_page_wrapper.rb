1ad1b9432a600d4511133abe3e604318048ff702
Christoph Bünte <info@christophbuente.de>
2013-09-02 15:49:06 +0200

    Add sidebar to static pages.

--- app/overrides/pages/add_id_to_page_wrapper.rb
+++ app/overrides/pages/add_id_to_page_wrapper.rb
@@ -3,3 +3,13 @@ Deface::Override.new(:virtual_path => 'spree/pages/show',
                      :surround => "code[erb-loud]:contains('spree/shared/content')",
                      :text => "<div id='page'><%= render_original %></div>"
                     )
+
+Deface::Override.new(:virtual_path => 'spree/pages/show',
+                     :name => 'add_sidebar_to_static_page',
+                     :insert_after => 'div#page',
+                     :erb => '<%- content_for :sidebar do -%>
+                      <div data-hook="homepage_sidebar_navigation">
+                        <%= render :partial => "spree/schweine/sidebar" %>
+                      </div>
+                    <%- end -%>'
+)
