addb45e6dd0bc9c807c42676d9c81f2389480e78
Christoph Bünte <info@christophbuente.de>
2013-09-02 14:33:44 +0200

    Use better selector.

--- app/overrides/pages/add_id_to_page_wrapper.rb
+++ app/overrides/pages/add_id_to_page_wrapper.rb
@@ -1,8 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/pages/show',
                      :name => 'add_id_to_page_wrapper',
-                     :add_to_attributes => "div",
-                     :attributes => {
-                       :id => 'page',
-                       :class => "eleven columns"
-                    })
-
+                     :surround => "code[erb-loud]:contains('spree/shared/content')",
+                     :text => "<div id='page'><%= render_original %></div>"
+                    )
