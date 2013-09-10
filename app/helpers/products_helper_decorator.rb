78647e4b5be6fba98e851539d71521225e895026
Christoph Bünte <info@christophbuente.de>
2013-09-10 11:38:16 +0200

    Render weight of pig correctly.

--- app/helpers/products_helper_decorator.rb
+++ app/helpers/products_helper_decorator.rb
@@ -1,6 +1,18 @@
 Spree::ProductsHelper.class_eval do
 
-  def number_to_weight(weight)
-    number_to_human(weight.to_i, :precision => 4, :units => {:unit => 'g', :thousand => 'kg'} )
+  def number_to_weight(weight, options = {})
+    options.reverse_merge!({:precision => 4, :units => {:unit => 'g', :thousand => 'kg', :billion => 't'}})
+    number_to_human(weight.to_i, options)
+  end
+
+  def render_weight(pig, options={})
+    html = ''
+    unless pig.weight.blank?
+      inner_html = ''
+      inner_html << content_tag( :b, 'Gewicht: ')
+      inner_html << content_tag( :span, number_to_weight(pig.weight, options), itemprop: "weight")
+      html << content_tag( :span, inner_html.html_safe, class: 'weight')
+    end
+    html
   end
 end
