be188e7ac1b1767abb8ea2342e502a9f3ff473e2
Christoph Bünte <info@christophbuente.de>
2013-09-27 14:21:44 +0200

    Show products in three colums.

--- app/overrides/surround_data_hook_products_list_item.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_products',
-                     :name => 'surround_data_hook_products_list_item',
-                     :surround_contents => '[data-hook="products_list_item"]',
-                     :text => '<div class="product"><%= render_original %></div>',
-                     :original => 'de2693a76d336b10c7896a252860365447ca4ddb',
-                     :sequence => 1)
