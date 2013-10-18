f676cc720b72c4bf3fefd76127afc863748b024b
Christoph Bünte <info@christophbuente.de>
2013-10-18 15:51:35 +0200

    Check if blog is installed and if there are any live posts.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -7,7 +7,7 @@ class Spree::StartController < Spree::StoreController
   def index
     @schweine = Spree::Schwein.order('position ASC').limit(3)
     @searcher = Spree::Config.searcher_class.new({per_page:3})
-    @blog = defined?(Spree::Blog) ? "Spree::Blog".constantize.first : nil
+    @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
     @products = @searcher.retrieve_products[0...3]
   end
 
