81f806c02fb73e1985e26462ef9793f0f2ad2875
Christoph Bünte <info@christophbuente.de>
2013-10-28 16:50:15 +0100

    Fix path for partial.

--- app/overrides/cart/make_error_message_html_safe.rb
+++ app/overrides/cart/make_error_message_html_safe.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/shared/error_messages',
+Deface::Override.new(:virtual_path => 'spree/shared/_error_messages',
                      :name => 'make_error_message_html_safe',
                      :replace => "li",
                      :erb => '<li><%= msg.html_safe %></li>' )
