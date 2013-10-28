cd48935ec4537d42fd6a848a61f1423b4a72e578
Christoph Bünte <info@christophbuente.de>
2013-10-28 13:39:54 +0100

    Sort products by count on hand on start and products page.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -7,8 +7,8 @@ class Spree::StartController < Spree::StoreController
   def index
     @schweine = Spree::Schwein.order('position ASC').limit(3)
     @searcher = Spree::Config.searcher_class.new({per_page:3})
+    @products = @searcher.retrieve_products.sort{|o| o.on_hand }.reverse[0...3]
     @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
-    @products = @searcher.retrieve_products[0...3]
   end
 
 end
---
+++ app/overrides/products/index/order_products_by_count_on_hand.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+Deface::Override.new(:virtual_path => 'spree/products/index',
+                    :name => 'order_products_by_count_on_hand',
+                    :replace => "code[erb-loud]:contains('spree/shared/products')",
+                    :erb => "<%= render :partial => 'spree/shared/products', :locals => { :products => @products.sort_by{|o| o.on_hand}.reverse, :taxon => @taxon } %>")
+
+
+
+
