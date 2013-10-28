65a9aaf5ce568977e6868a584ab10e7a303874d7
Christoph Bünte <info@christophbuente.de>
2013-10-28 14:36:48 +0100

    Update search for start page.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -6,7 +6,7 @@ class Spree::StartController < Spree::StoreController
 
   def index
     @schweine = Spree::Schwein.order('position ASC').limit(3)
-    @searcher = Spree::Config.searcher_class.new({per_page:3})
+    @searcher = Spree::Config.searcher_class.new({})
     @products = @searcher.retrieve_products.sort{|o| o.on_hand }.reverse[0...3]
     @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
   end
