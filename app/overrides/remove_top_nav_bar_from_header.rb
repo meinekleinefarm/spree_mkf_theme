78d18c6bc3efee296131ff29073311f0b8cb1541
Christoph Bünte <info@christophbuente.de>
2013-12-23 10:25:40 +0100

    Make topnavbar invisible.

--- app/overrides/remove_top_nav_bar_from_header.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
-                     :name => 'remove_top_nav_bar_from_header',
-                     :remove => "#top-nav-bar",
-                     :original => '5a2a29224ad730413a4740085278d3ddb0ceebf1',
-                     :sequence => 1)
