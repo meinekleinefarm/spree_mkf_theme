cc46f8153c802e2791533f8b0d684d1f1af4bb5c
Christoph Bünte <info@christophbuente.de>
2014-01-17 15:56:09 +0100

    Adopt rebased code to new deface syntax.

--- app/overrides/checkout/rename_final_button.rb
+++ app/overrides/checkout/rename_final_button.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => 'spree/checkout/_payment',
                     :name => 'rename_final_button',
-                    :replace => "code[erb-loud]:contains('submit_tag')",
+                    :replace => "code[loud]:contains('submit_tag')",
                     :erb => "<%= submit_tag( t(:save_and_finish), :class => 'continue button primary') %>"
                     )
 
