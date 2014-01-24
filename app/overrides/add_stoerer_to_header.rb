63bc1fa1232861513276a77adcdde3d1d8462e91
Christoph Bünte <info@christophbuente.de>
2014-01-24 11:40:06 +0100

    Add header störer via Header Menu bar.

--- app/overrides/add_stoerer_to_header.rb
+++ app/overrides/add_stoerer_to_header.rb
@@ -1,5 +1,5 @@
-# Deface::Override.new(:virtual_path => 'spree/shared/_header',
-#                      :name => 'add_stoerer_to_header',
-#                      :insert_after => 'figure',
-#                      :erb => "<%= link_to(image_tag('/assets/store/gans.png', :id => 'stoerer'), 'http://www.meinekleinefarm.org/news/ganse-mit-geschicht')",
-#                      :sequence => {:after => 'surround_header_content_with_container'})
+Deface::Override.new(:virtual_path => 'spree/shared/_header',
+                    :name => 'add_stoerer_to_header',
+                    :insert_after => 'figure',
+                    :partial => "spree/shared/header_stoerer",
+                    :sequence => {:after => 'surround_header_content_with_container'})
