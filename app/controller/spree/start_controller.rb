62652864f236cbb80b3f0a03ffb1efaec01f5c1f
Christoph Bünte <info@christophbuente.de>
2014-03-07 16:38:17 +0100

    Do not show misc products on start page.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -7,7 +7,9 @@ class Spree::StartController < Spree::StoreController
   def index
     @schweine = Spree::Schwein.order('position ASC').limit(3)
     @searcher = Spree::Config.searcher_class.new({})
-    @products = @searcher.retrieve_products.sort{|o| o.on_hand }.reverse[0...3]
+    @products = @searcher.retrieve_products.
+      reject{|p| p.taxons.any?{|t| t.name.include?('Sonstiges')}}.
+      sort{|p| p.on_hand }.reverse[0...3]
     @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
   end
 
