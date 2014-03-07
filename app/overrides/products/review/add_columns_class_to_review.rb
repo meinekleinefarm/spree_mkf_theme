4a11cc7a9e44cdc285ae83f06432bf15889254cd
Christoph Bünte <info@christophbuente.de>
2014-03-07 11:45:42 +0100

    Show just 3 reviews in a row and make stars yellow again.

--- app/overrides/products/review/add_columns_class_to_review.rb
+++ app/overrides/products/review/add_columns_class_to_review.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_review',
                      :name => 'add_columns_class_to_review',
                      :set_attributes => 'div.review',
                      :attributes => {
-                       :class => "review columns four <%= cycle('alpha', '', '', 'omega')%>"
+                       :class => "review columns four <%= cycle('alpha', '', 'omega')%>"
                       })
 
 
