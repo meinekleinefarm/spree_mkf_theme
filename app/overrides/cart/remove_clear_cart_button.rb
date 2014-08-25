cf9bf0ab87149a3f6bb29dd30668159d37cfbe70
Christoph Bünte <info@christophbuente.de>
2014-08-25 13:00:53 +0200

    Reactivate empty cart button.

--- app/overrides/cart/remove_clear_cart_button.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/orders/edit',
-                     :name => 'remove_clear_cart_button',
-                     :replace_contents => "p#clear_cart_link",
-                     :erb => "<%= link_to t(:continue_shopping), products_path, :class => 'continue button gray' %>")
