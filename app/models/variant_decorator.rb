c23be3c823a184d4afc0844ac904a3bdf5f0bfd1
Christoph Bünte <info@christophbuente.de>
2014-09-01 11:31:24 +0200

    Make attribute accessible.

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -3,7 +3,9 @@ Spree::Variant.class_eval do
 
   before_save :set_or_update_initial_count
 
+  attr_accessible :initial_count_on_hand
+
   def set_or_update_initial_count
-    initial_count_on_hand = [initial_count_on_hand.to_i, count_on_hand.to_i].max
+    self.initial_count_on_hand = [self.initial_count_on_hand.to_i, self.count_on_hand.to_i].max
   end
 end
