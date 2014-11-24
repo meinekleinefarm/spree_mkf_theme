4e0c33036f581004150738a989025bd7e6ba7e5c
Christoph Bünte <info@christophbuente.de>
2014-11-24 12:15:24 +0100

    Include product assemblies in product list.

--- app/controller/products_controller_decorator.rb
+++ app/controller/products_controller_decorator.rb
@@ -2,7 +2,13 @@ Spree::ProductsController.class_eval do
   respond_override :index => {
     :html => {
       :success => lambda do
-        @products.sort!{|a,b| b.available_on <=> a.available_on rescue nil }
+        @products.all
+        if defined? Spree::AssembliesPart == 'constant'
+          @assemblies = Spree::AssembliesPart.all.map(&:assembly).uniq.select{|a| a.on_hand > 0 }
+        else
+          @assemblies = []
+        end
+        @products = (@products.all + @assemblies).uniq.sort{|a,b| b.available_on <=> a.available_on rescue nil }
         render 'spree/products/index.html.erb'
       end
     }
