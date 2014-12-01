afe0a201c176f913fa676564370901992c666dcc
Christoph Bünte <info@christophbuente.de>
2014-12-01 21:55:48 +0100

    Put assemblies on the start page too.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -7,9 +7,14 @@ class Spree::StartController < Spree::StoreController
   def index
     @schweine = Spree::Schwein.order('position ASC').limit(3)
     @searcher = Spree::Config.searcher_class.new({})
+    if ('Spree::AssembliesPart'.constantize rescue false)
+      @assemblies = Spree::AssembliesPart.all.map(&:assembly).uniq.select{|a| a.on_hand > 0 }
+    else
+      @assemblies = []
+    end
     @products = @searcher.retrieve_products.
-      reject{|p| p.taxons.any?{|t| t.name.include?('Sonstiges')}}.
-      sort{|a,b| b.available_on <=> a.available_on rescue nil}[0...3]
+      reject{|p| p.taxons.any?{|t| t.name.include?('Sonstiges')}}
+    @products = (@products + @assemblies).uniq.sort{|a,b| b.available_on <=> a.available_on rescue nil}[0...3]
     @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
   end
 
