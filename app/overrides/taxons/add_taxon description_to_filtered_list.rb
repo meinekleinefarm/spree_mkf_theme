2ee0050342a8e60c2820ef8320debd31aecfc639
Christoph Bünte <info@christophbuente.de>
2015-02-08 22:02:41 +0100

    Show real html instead of simple formatted stuff.

--- app/overrides/taxons/add_taxon description_to_filtered_list.rb
+++ app/overrides/taxons/add_taxon description_to_filtered_list.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => 'spree/taxons/show',
                     :name => 'add_taxon description_to_filtered_list',
                     :insert_after => 'h1',
                     :original => 'ea93a4c45e39063b5a054c65d6f9a762d9890fd3',
-                    :erb => '<%= simple_format(strip_tags(@taxon.description)) %>')
+                    :erb => '<%= @taxon.description.html_safe %>')
