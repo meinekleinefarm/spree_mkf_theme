6c5dfdb0970802dd9633c657563674ca267eaa5b
Christoph Bünte <info@christophbuente.de>
2015-02-08 22:18:44 +0100

    Fix a nil pointer for taxon description.

--- app/overrides/taxons/add_taxon description_to_filtered_list.rb
+++ app/overrides/taxons/add_taxon description_to_filtered_list.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => 'spree/taxons/show',
                     :name => 'add_taxon description_to_filtered_list',
                     :insert_after => 'h1',
                     :original => 'ea93a4c45e39063b5a054c65d6f9a762d9890fd3',
-                    :erb => '<%= @taxon.description.html_safe %>')
+                    :erb => '<%= @taxon.description.try(:html_safe) %>')
