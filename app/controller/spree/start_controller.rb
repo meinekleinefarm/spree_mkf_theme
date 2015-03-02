51884699575e44ac2d7cc247c1cbad9f0f0778c6
Christoph Bünte <info@christophbuente.de>
2015-03-02 23:44:58 +0100

    Show 2 different lists for in-stock and out-of-stock photos.

--- app/controller/spree/start_controller.rb
+++ app/controller/spree/start_controller.rb
@@ -13,7 +13,7 @@ class Spree::StartController < Spree::StoreController
       @assemblies = []
     end
     @products = @searcher.retrieve_products.
-      reject{|p| p.taxons.any?{|t| t.name.include?('Sonstiges')}}
+      reject{|p| !p.has_stock? || p.taxons.any?{|t| t.name.include?('Sonstiges')}}
     @products = (@products + @assemblies).uniq.sort{|a,b| b.available_on <=> a.available_on rescue nil}[0...3]
     @blog = defined?(SpreeEssentialBlog) == 'constant' ? "Spree::Blog".constantize.first : nil
   end
