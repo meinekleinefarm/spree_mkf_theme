a7bf07e03b6a1a4ee8553bce8de5c77b81ed89aa
Christoph Bünte <info@christophbuente.de>
2015-11-08 22:33:48 +0100

    Bring back social icons in header.

--- app/assets/javascripts/store/spree_mkf_theme.js
+++ app/assets/javascripts/store/spree_mkf_theme.js
@@ -13,4 +13,16 @@ $(document).ready(function() {
       $(this).after('<figcaption>' + $(this).attr("alt") + '</figcaption>');
     }
   });
+
+  $('<div id="icons" \
+      <a href="https://www.facebook.com/meinekleinefarm" target="_blank" title="Follow us on facebook"> \
+        <img src="/assets/icon-facebook.png" alt="facebook icon" ></img> \
+      </a> \
+      <a href="https://www.pinterest.com/meinekleinefarm/" target="_blank" title="Follow us on pinterest"> \
+        <img src="/assets/icon-pinterest.png" alt="pinterest icon" ></img> \
+      </a> \
+      <a href="/newsletter" title="Sign up for our awesome newsletter"> \
+        <img src="/assets/icon-mail.png" alt="newsletter icon" ></img> \
+      </a> \
+    </div>').insertAfter('figure#logo')
 });
